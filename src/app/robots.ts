7dbd25ad98fdfc2df821f3c06a0e09f4b37ae268
Daniel Leszczynski <daniel.leszczynski.extlm@lastminute.com>
2026-04-02 16:13:46 +0200

    feat: update robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,20 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://vroomdealer.pl";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      }
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
